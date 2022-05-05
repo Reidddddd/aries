65c0f40337b2252f71f0c7de65f129892ee53506
Reid Chan <reidchan@apache.org>
2022-05-05 16:22:32 +0800

    Add GetWorkload

---
+++ aries-hbase/src/main/java/org/apache/aries/GetWorkload.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2019 R.C
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.aries;
+
+import org.apache.aries.common.BaseWorkload;
+import org.apache.aries.common.BoolParameter;
+import org.apache.aries.common.Constants;
+import org.apache.aries.common.IntParameter;
+import org.apache.aries.common.KEY_PREFIX;
+import org.apache.aries.common.LongParameter;
+import org.apache.aries.common.Parameter;
+import org.apache.aries.factory.GetHandlerFactory;
+import org.apache.aries.factory.HandlerFactory;
+import org.apache.aries.factory.PutHandlerFactory;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class GetWorkload extends BaseWorkload {
+
+  private final Parameter<Boolean> random_ops =
+      BoolParameter.newBuilder(getParameterPrefix() + ".random_key", true)
+                   .setDescription("It must be set false, if using SEQ key mode").opt();
+  private final Parameter<Boolean> result_verification =
+      BoolParameter.newBuilder(getParameterPrefix() + ".result_verification", false)
+                   .setDescription("If set true, there will be verification for the returned results").opt();
+
+  @Override
+  public void requisite(List<Parameter> requisites) {
+    super.requisite(requisites);
+    requisites.add(random_ops);
+    requisites.add(result_verification);
+  }
+
+  @Override
+  protected void exampleConfiguration() {
+    super.exampleConfiguration();
+    example(random_ops.key(), "true");
+    example(result_verification.key(), "false");
+  }
+
+  @Override
+  protected void midCheck() {
+    super.midCheck();
+    KEY_PREFIX prefix = (KEY_PREFIX) key_kind.value();
+    if (prefix != KEY_PREFIX.SEQ) {
+      throw new IllegalArgumentException("If must be set SEQ key kind when using GetWorkload");
+    }
+  }
+
+  @Override
+  protected HandlerFactory initHandlerFactory(ToyConfiguration configuration, List<Parameter> parameters) {
+    return new GetHandlerFactory(configuration, parameters);
+  }
+
+  @Override protected String getParameterPrefix() {
+    return "gw";
+  }
+
+}
---
+++ aries-hbase/src/main/java/org/apache/aries/factory/GetHandlerFactory.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2019 R.C
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.aries.factory;
+
+import com.codahale.metrics.Timer;
+import org.apache.aries.ToyConfiguration;
+import org.apache.aries.common.Parameter;
+import org.apache.aries.common.VALUE_KIND;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GetHandlerFactory extends HandlerFactory {
+
+  public GetHandlerFactory(ToyConfiguration configuration, List<Parameter> parameters) {
+    super(configuration, parameters);
+
+    for (Parameter parameter : parameters) {
+      if (parameter.key().contains(GetHandler.RANDOM_OPS))  hbase_conf.setBoolean(GetHandler.RANDOM_OPS, (Boolean) parameter.value());
+      if (parameter.key().contains(GetHandler.RESULT_VERIFICATION))  hbase_conf.setBoolean(GetHandler.RESULT_VERIFICATION, (Boolean) parameter.value());
+    }
+  }
+
+  @Override
+  public BaseHandler createHandler(TableName table) throws IOException {
+    return new GetHandler(hbase_conf, table);
+  }
+
+  public static class GetHandler extends BaseHandler {
+
+    public static final String RANDOM_OPS = "random_key";
+
+    public static final String RESULT_VERIFICATION = "result_verification";
+
+    GetHandler(Configuration conf, TableName table) throws IOException {
+      super(conf, table);
+    }
+
+    @Override
+    public void run() {
+      try {
+        Table target_table = connection.getTable(getTable());
+        while (!isInterrupted()) {
+          try (final Timer.Context context = latency.time()) {
+            String key = getKey(key_kind, key_length, hbase_conf.getBoolean(RANDOM_OPS, true));
+
+            Get get = new Get(Bytes.toBytes(key));
+            get.addColumn(Bytes.toBytes(family), Bytes.toBytes("q"));
+            get.setCacheBlocks(false);
+
+            Result result = target_table.get(get);
+            byte[] value = result.getValue(Bytes.toBytes(family), Bytes.toBytes("q"));
+            if (hbase_conf.getBoolean(RESULT_VERIFICATION, false)) {
+              if (value_kind == VALUE_KIND.FIXED) {
+                if (verifiedResult(value_kind, key, value)) {
+                } else {
+                }
+              }
+            }
+          }
+        }
+      } catch (Exception e) {
+        LOG.warning("Error occured " + e.getMessage());
+      }
+    }
+
+  }
+
+}
