ee48b12df65cdeda2306764c94846c418032d14b
Reid Chan <reidchan@apache.org>
2022-10-19 15:43:27 +0800

    ChaosAction: SnapshotTable

---
+++ aries-hbase/src/main/java/org/apache/aries/action/SnapshotTable.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2019 R.C
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.aries.action;
+
+import org.apache.hadoop.hbase.TableName;
+
+public class SnapshotTable extends TableBase {
+
+  public SnapshotTable() {}
+
+  private String snapshot_name;
+
+  @Override
+  protected void perform(TableName table) throws Exception {
+    admin.snapshot(snapshot_name, table);
+  }
+
+  @Override
+  protected void prePerform(TableName table) throws Exception {
+    snapshot_name = table.getNameAsString().replaceAll(":", "_") + System.currentTimeMillis();
+    admin.snapshot(snapshot_name, table);
+  }
+
+  @Override
+  protected void postPerform(TableName table) throws Exception {
+  }
+
+}
