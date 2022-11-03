dc4c5a9eb970915577af181d27ba5b3820caed3c
Reid Chen <reidchan@apache.org>
2022-11-03 13:48:57 +0800

    ChaosAction: AlterMemstoreFlushSize

---
+++ aries-hbase/src/main/java/org/apache/aries/chaos/AlterMemstoreFlushSize.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2019 R.C
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.aries.chaos;
+
+import org.apache.aries.common.Constants;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+
+public class AlterMemstoreFlushSize extends AlterBase {
+
+  private final static long AT_LEAST_SIZE = Constants.ONE_MB * 10;
+
+  private long flush_size;
+  private HTableDescriptor descriptor;
+
+  public AlterMemstoreFlushSize() {}
+
+  protected void alter(TableName table, HColumnDescriptor family) throws Exception {
+    flush_size = RANDOM.nextLong();
+    if (flush_size < AT_LEAST_SIZE) {
+      flush_size = AT_LEAST_SIZE;
+    }
+    descriptor.setMemStoreFlushSize(flush_size);
+    admin.modifyTable(table, descriptor);
+  }
+
+  protected void preAlter(TableName table, HColumnDescriptor family) throws Exception {
+    super.preAlter(table, family);
+    descriptor = admin.getTableDescriptor(table);
+    flush_size = descriptor.getMemStoreFlushSize();
+    LOG.info(preLogMessage("max file size", table, family, flush_size));
+  }
+
+  protected void postAlter(TableName table, HColumnDescriptor family) throws Exception {
+    super.postAlter(table, family);
+    LOG.info(postLogMessage("block size", table, family, flush_size));
+  }
+
+}
